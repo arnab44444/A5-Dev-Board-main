a78364cb31e3045dbca7a3a639b14edef5c136be
arnab44444 <arnabbiswas661@gmail.com>
2025-03-02 22:16:33 +0600

    mfinal

--- js/script.js
+++ js/script.js
@@ -86,19 +86,10 @@ for(let i = 0; i < btn.length; i++){
 //5. theme button
 
 // Select the theme button
-const themeBtn = document.getElementById("theme-btn");
+// const themeBtn = document.getElementById("theme-btn");
 
-// Function to generate a random color
-function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-// Add click event listener
-themeBtn.addEventListener("click", function() {
-    document.body.style.backgroundColor = getRandomColor();
-});
+document.getElementById("theme-btn").onclick = function () {
+    let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    document.documentElement.style.backgroundColor = randomColor;
+    document.body.style.backgroundColor = randomColor;
+};
